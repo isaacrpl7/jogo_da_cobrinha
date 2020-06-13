f987bd8ec6ac6724892c402e2e0af537c8463dc2
Isaac Reinaldo Pinheiro de Lima <50757880+isaacrpl7@users.noreply.github.com>
2020-06-13 12:44:27 -0300

    Adicionei estilo ao index

--- js/script.js
+++ js/script.js
@@ -45,11 +45,18 @@ function iniciarJogo() {//esse código irá rodar varias vezes a cada 100ms
      * criar duas variáveis para guardarem o valor inicial x e y da cobrinha
      * fazer condicionais para a nova direção da cobrinha
      * remover do array um elemento para depois adicionar o novo
-     */
+    */
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :c');
+        }
+    }
     
     criarBG();
     criarCobrinha();
