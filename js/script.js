860502ce51466cdcac806f94b35bfcbd64a257c1
Isaac Reinaldo Pinheiro de Lima <50757880+isaacrpl7@users.noreply.github.com>
2020-06-13 12:15:02 -0300

    Cobrinha não sai do canvas e controle
    Adicionei o controle da cobrinha pelo teclado, e ela não sai mais do canvas.

--- js/script.js
+++ js/script.js
@@ -20,7 +20,28 @@ function criarCobrinha() {
     }
 }
 
-function iniciarJogo() {
+document.addEventListener('keydown', update);
+
+function update(event) {//mudança de direção conforme a tecla
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo() {//esse código irá rodar varias vezes a cada 100ms
+    /**
+     * Vai fazer condicionais para caso a cobrinha saia do plano
+     * Vai desenhar o background e desenhar a cobrinha,
+     * criar duas variáveis para guardarem o valor inicial x e y da cobrinha
+     * fazer condicionais para a nova direção da cobrinha
+     * remover do array um elemento para depois adicionar o novo
+     */
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
     criarBG();
     criarCobrinha();
 
